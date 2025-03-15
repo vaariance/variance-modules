287cfc31ba5a08fa699f8db2108bf46449380f93
Godwin Alexander Ekainu <clsyfriday@gmail.com>
2025-03-15 10:01:08 +0100

    refactor

--- lib/variance_modules.dart
+++ lib/variance_modules.dart
@@ -5,12 +5,6 @@ import 'dart:typed_data';
 import 'package:variance_dart/variance_dart.dart';
 import 'package:web3_signers/web3_signers.dart';
 import 'package:web3dart/crypto.dart';
-import 'package:web3dart/web3dart.dart';
-
-import 'src/7579/StandardModuleInterface/interface.dart'
-    show ValidatorModuleInterface;
-import 'src/7579/modules/validators/SocialRecovery/social_recovery.m.dart'
-    show SocialRecoveryContract, social_recovery_abi;
 
 part 'src/7579/constants.dart';
 part 'src/7579/safe.dart';
