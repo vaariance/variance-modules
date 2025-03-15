fa59c425f478a438cd155321c47e044ff768de30
Godwin Alexander Ekainu <clsyfriday@gmail.com>
2025-03-15 11:26:03 +0100

    refactor

--- lib/src/7579/safe.dart
+++ lib/src/7579/safe.dart
@@ -2,7 +2,7 @@
 
 part of '../../variance_modules.dart';
 
-class _Safe7579Initializer extends _SafeInitializer {
+class _Safe7579Initializer extends BaseSafeInitializer {
   final EthereumAddress launchpad;
   final Iterable<ModuleInit>? validators;
   final Iterable<ModuleInit>? executors;
@@ -23,8 +23,12 @@ class _Safe7579Initializer extends _SafeInitializer {
     this.hooks,
     this.attesters,
     this.attestersThreshold,
+    super.encodeWebauthnSetup,
   });
 
+  /// Generates the launchpad initialization data.
+  ///
+  /// Returns a [Uint8List] containing the encoded initialization data.
   Uint8List getLaunchpadInitData() {
     return encode7579LaunchpadInitdata(
         launchpad: launchpad,
@@ -47,61 +51,5 @@ class _Safe7579Initializer extends _SafeInitializer {
         module: module,
         singleton: singleton);
     return encode7579InitCalldata(launchpad: launchpad, initHash: initHash);
-  }
-}
-
-class _SafeInitializer {
-  final Iterable<EthereumAddress> owners;
-  final int threshold;
-  final Safe4337ModuleAddress module;
-  final SafeSingletonAddress singleton;
-  final Uint8List Function(Uint8List Function())? encodeWebauthnSetup;
-
-  _SafeInitializer({
-    required this.owners,
-    required this.threshold,
-    required this.module,
-    required this.singleton,
-    this.encodeWebauthnSetup,
-  });
-
-  /// Generates the initializer data for deploying a new Safe contract.
-  ///
-  /// Returns a [Uint8List] containing the encoded initializer data.
-  Uint8List getInitializer() {
-    encodeModuleSetup() {
-      return Contract.encodeFunctionCall(
-          "enableModules", module.setup, ContractAbis.get("enableModules"), [
-        [module.address]
-      ]);
-    }
-
-    final setup = {
-      "owners": owners.toList(),
-      "threshold": BigInt.from(threshold),
-      "to": null,
-      "data": null,
-      "fallbackHandler": module.address,
-    };
-
-    if (encodeWebauthnSetup != null) {
-      setup["to"] = Addresses.safeMultiSendaddress;
-      setup["data"] = encodeWebauthnSetup!(encodeModuleSetup);
-    } else {
-      setup["to"] = module.setup;
-      setup["data"] = encodeModuleSetup();
-    }
-
-    return Contract.encodeFunctionCall(
-        "setup", singleton.address, ContractAbis.get("setup"), [
-      setup["owners"],
-      setup["threshold"],
-      setup["to"],
-      setup["data"],
-      setup["fallbackHandler"],
-      Addresses.zeroAddress,
-      BigInt.zero,
-      Addresses.zeroAddress,
-    ]);
   }
 }
